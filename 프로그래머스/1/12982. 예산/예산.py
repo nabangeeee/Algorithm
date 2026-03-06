db694ee993957590352813d3824c0d0c66c0d3d6
Nahee <nahee4134@gmail.com>
2026-03-06 12:57:16 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,14 @@
+def solution(d, budget):
+    d.sort()
+    
+    total = 0
+    cnt = 0
+    
+    for i in d:
+        total += i
+        if total <= budget:
+            cnt += 1
+        else:
+            total -= i
+                
+    return cnt
